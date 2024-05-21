43b2c3e5182e33630f671643ab81c7d552922065
unknown <vinicius.teixeira65@etec.sp.gov.br>
2024-05-21 10:44:46 -0300

    alteracao em TaskList

--- src/screens/Task_List.js
+++ src/screens/Task_List.js
@@ -1,11 +1,18 @@
 import React, {Component} from "react"
-import {StyleSheet, View, Text} from "react-native"
+import {StyleSheet, View, Text, ImageBackground} from "react-native"
+
+import today_Image from "../../assets/imgs/today.jpg"
 
 export default class TaskList extends Component{
     render(){
         return(
             <View style={styles.container}>
-                <Text>Lista de Tarefas</Text>
+               <ImageBackground source={today_Image} style={styles.background}>
+
+               </ImageBackground> 
+               <View style={styles.TaskList}>
+
+               </View>
             </View>
         )
     }
@@ -14,6 +21,12 @@ export default class TaskList extends Component{
 const styles = StyleSheet.create({
     container: {
         flex: 1
+    },
+    background:{
+        flex: 3
+    },
+    TaskList:{
+        flex: 7
     }
 
     
